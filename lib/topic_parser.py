55377a0d4271b139f65319e29f2e364c6d40b753
jackscotti <jacopojackscotti@gmail.com>
2016-01-19 22:43:21 +0000

    Return total number of docs tagged to subtopic

--- lib/topic_parser.py
+++ lib/topic_parser.py
@@ -1,7 +1,11 @@
 import requests
 
+def subtopic_slug(subtopic):
+    return subtopic[7:]
+
 BASE_URL = 'https://www.gov.uk/api/content'
 TOPICS_PATH = '/topic'
+SEARCH_BASE_URL = 'https://www.gov.uk/api/search.json?filter_specialist_sectors='
 
 home = requests.get(BASE_URL + TOPICS_PATH).json()
 
@@ -9,7 +13,7 @@ topics = home['links']['children']
 
 topics_with_subtopics = []
 
-print("Topic,Subtopic")
+print("Topic,Subtopic,Total tagged documents")
 for topic in topics:
     topic_base_path = topic['base_path']
 
@@ -19,9 +23,16 @@ for topic in topics:
 
     for subtopic in subtopics:
         subtopic_base_path = subtopic["base_path"]
-        print(topic_base_path + "," + subtopic_base_path)
+
+        tagged_docs = requests.get(SEARCH_BASE_URL + subtopic_slug(subtopic_base_path)).json()
+        total_tagged_docs = tagged_docs["total"]
+
+        print(topic_base_path + "," + subtopic_base_path + "," + str(total_tagged_docs))
 
 '''
 Spreadsheet:
-https://docs.google.com/spreadsheets/d/1PtgivxT7aCrmgogS7yV2nEPlK12hkkTSzKMFwA-_k1I
+https://docs.google.com/spreadsheets/d/1uslE-JDztW696IvcMinhRXS0e2D9zi5cJssRENb9nKE
+
+TODO:
+calculate total documents tagged to a topic
 '''
