70da5c3366f57db90d74c42e69e8f8016ced2246
jackscotti <jacopojackscotti@gmail.com>
2016-02-07 19:46:21 +0000

    Create relationship between documents and subtopics
    Many to many.

--- db_populator.py
+++ db_populator.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from table_definition import Topic, Subtopic
+from table_definition import Topic, Subtopic, Document
 
 engine = create_engine('sqlite:///klassify.db', echo=True)
 
@@ -8,24 +8,30 @@ engine = create_engine('sqlite:///klassify.db', echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Examples:
-# create a topic
+### Examples:
+# create a topic, subtopic and document
 # test_topic = Topic(name="HMRC", slug="/hmrc")
-# create a subtopic
 # test_subtopic = Subtopic(name="HMRC payments", slug="/payments")
+# test_document = Document(name="Self assessment deadlines", slug="/self-assessment", html="<strong>PAY NOW</strong>")
 
-# add subtopic to topic
-# test_topic.subtopics = [test_subtopic]
+# create relationships
+# test_topic.subtopics    = [test_subtopic]
+# test_subtopic.documents = [test_document]
 
 # add topic to session
-# session.add_all([test_topic, test_subtopic])
+# session.add_all([test_topic, test_subtopic, test_document])
 
 # commit session to db
 # session.commit()
 
-# Example: pull out first subtopic
+### Examples:
+# pull out first subtopic
 # session.query(Subtopic).first()
-# Example: pull out first topic and ask subtopics
+# pull out first topic and ask subtopics
 # session.query(Topic).first().subtopics
+# pull out first document and ask subtopics
+# session.query(Document).first().subtopics
+# pull out first subtopic and ask documents
+# session.query(Subtopic).first().documents
 
 import pdb; pdb.set_trace()
--- table_definition.py
+++ table_definition.py
@@ -16,15 +16,25 @@ class Topic(Base):
     def __repr__(self):
         return "<User(name='%s', slug='%s')>" % (self.name, self.slug)
 
+# create association table (subtopic-documents)
+subtopics_documents = Table('subtopics_documents', Base.metadata,
+    Column('subtopic_id', ForeignKey('subtopics.id'), primary_key=True),
+    Column('document_id', ForeignKey('documents.id'), primary_key=True)
+)
+
 class Subtopic(Base):
     __tablename__ = 'subtopics'
 
     id   = Column(Integer, primary_key=True)
     name = Column(String)
     slug = Column(String)
-    topic_id = Column(Integer, ForeignKey('topics.id'))
-    topic = relationship("Topic", back_populates="subtopics")
 
+    topic_id  = Column(Integer, ForeignKey('topics.id'))
+    topic     = relationship("Topic", back_populates="subtopics")
+
+    documents = relationship(
+        "Document", secondary=subtopics_documents, back_populates="subtopics"
+    )
 
     def __repr__(self):
         return "<User(name='%s', slug='%s')>" % (self.name, self.slug)
@@ -33,6 +43,27 @@ class Subtopic(Base):
 Topic.subtopics = relationship(
     "Subtopic", order_by=Subtopic.id, back_populates="topic"
 )
+
+class Document(Base):
+    __tablename__ = 'documents'
+
+    id   = Column(Integer, primary_key=True)
+    name = Column(String)
+    slug = Column(String)
+    html = Column(Text)
+
+    subtopics = relationship(
+        'Subtopic', secondary=subtopics_documents, back_populates='documents'
+    )
+
+    def __init__(self, name, slug, html):
+        self.name = name
+        self.slug = slug
+        self.html = html
+
+    def __repr__(self):
+        return "Document(%r, %r, %r)" % (self.name, self.slug, self.html)
+
 # Create tables in database
 # always delete db when modifying this
 Base.metadata.create_all(engine)
