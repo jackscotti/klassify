7290976e7c69c1e4f7eb1fd1399da016ff9e9878
jackscotti <jacopojackscotti@gmail.com>
2016-02-07 18:54:52 +0000

    Connect topics to subtopics
    One to many relationship.

--- db_populator.py
+++ db_populator.py
@@ -13,12 +13,19 @@ session = Session()
 # test_topic = Topic(name="HMRC", slug="/hmrc")
 # create a subtopic
 # test_subtopic = Subtopic(name="HMRC payments", slug="/payments")
+
+# add subtopic to topic
+# test_topic.subtopics = [test_subtopic]
+
 # add topic to session
 # session.add_all([test_topic, test_subtopic])
+
 # commit session to db
 # session.commit()
 
 # Example: pull out first subtopic
 # session.query(Subtopic).first()
+# Example: pull out first topic and ask subtopics
+# session.query(Topic).first().subtopics
 
 import pdb; pdb.set_trace()
--- table_definition.py
+++ table_definition.py
@@ -12,7 +12,6 @@ class Topic(Base):
     id   = Column(Integer, primary_key=True)
     name = Column(String)
     slug = Column(String)
-    # TODO: add subtopics
 
     def __repr__(self):
         return "<User(name='%s', slug='%s')>" % (self.name, self.slug)
@@ -23,11 +22,17 @@ class Subtopic(Base):
     id   = Column(Integer, primary_key=True)
     name = Column(String)
     slug = Column(String)
-    # TODO: add documents
+    topic_id = Column(Integer, ForeignKey('topics.id'))
+    topic = relationship("Topic", back_populates="subtopics")
+
 
     def __repr__(self):
         return "<User(name='%s', slug='%s')>" % (self.name, self.slug)
 
+# link topic to subtopics
+Topic.subtopics = relationship(
+    "Subtopic", order_by=Subtopic.id, back_populates="topic"
+)
 # Create tables in database
 # always delete db when modifying this
 Base.metadata.create_all(engine)
