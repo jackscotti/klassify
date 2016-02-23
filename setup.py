140b85aed8698f5d1b840e8f1a26d793d14304bf
jackscotti <jacopojackscotti@gmail.com>
2016-02-23 21:14:40 +0000

    Remove db if present when setting up the project

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from src.topic_importer import TopicImporter
 from src.document_importer import DocumentImporter
+import os
+
+database_name = "klassify"
+
+if os.path.exists("%s.db" % database_name):
+    os.remove("%s.db" % database_name)
 
 # Add topics and subtopics
 TopicImporter().run()
