d96c00b637a6ebb204f99792372a466aa4a9689e
jackscotti <jacopojackscotti@gmail.com>
2016-03-21 15:57:05 +0000

    Refactor classifier

--- classifier.py
+++ classifier.py
@@ -8,36 +8,33 @@ import string
 
 DBH = DBHandler(echo=False)
 
-topics = DBH.session.query(Topic).all()
-def find_documents_for_topic(topic):
-    documents = []
-    for subtopic in topic.subtopics:
-        for document in subtopic.documents:
-            documents.append(document)
 
-    return documents
 
-document_sets = []
-for topic in topics:
-    document_sets.append(find_documents_for_topic(topic))
+class WordProcessor():
 
-class Classifier():
     def __init__(self, documents):
         self.documents = documents
         self.stemmer = PorterStemmer()
-        self.words = []
-        self.features = []
+        self.vocabulary = self.make_vocabulary()
 
-    def extract_words(self):
+    def extract_words(self, document=None):
         '''
         Take all documents content, tokenize it and put it in a `contents` variable
         '''
-        for doc in self.documents:
+        if document:
+            documents = [document]
+        else:
+            documents = self.documents
+
+        words = []
+
+        for doc in documents:
             # tokenize
             tokens = word_tokenize(doc.content)
 
             for token in tokens:
-                self.words.append(token)
+                words.append(token)
+        return words
 
     def downcase_words(self, words):
         return [w.lower() for w in words]
@@ -56,42 +53,100 @@ class Classifier():
         # remove anything that is not a word (js script that haven't been removed by previous parsing)
         return [w for w in words if len(w) < 25]
 
-    def extract_features(self):
-        self.extract_words()
+    def remove_short_words(self, words):
+        filtered_words = []
+        for w in words:
+            if w.isalpha:
+                if len(w) > 1:
+                    filtered_words.append(w)
+            else:
+                filtered_words.append(w)
 
-        words = self.downcase_words(self.words)
-        words = self.remove_stopwords(words)
-        words = self.stem_words(words)
-        words = self.remove_long_words(words)
+        return filtered_words
 
-        return words
+    def make_vocabulary(self, document=None):
+        if document:
+            v = self.extract_words(document)
+        else:
+            v = self.extract_words()
 
-for document_set in document_sets:
-    print(document_set[0].title)
-    c = Classifier(document_set)
-    features = c.extract_features()
-    print("Number of features:", len(features))
-    # Convert in nltk frequency distribution object
-    all_words = nltk.FreqDist(features)
-    # print 15 most common words
-    print(all_words.most_common(15))
+        v = self.downcase_words(v)
+        v = self.remove_stopwords(v)
+        v = self.stem_words(v)
+        v = self.remove_short_words(v)
+        v = self.remove_long_words(v)
 
-    # top 3000 words
-    word_features = list(all_words.keys())[:3000]
+        return v
 
-def find_features(document):
-    words = set(document)
-    features = {}
-    for w in word_features:
-        features[w] = (w in words)
+    def bag_of_words(self, document):
+        vocabulary = self.top_words(self.freq_dist(self.vocabulary))
 
-    return features
+        doc_words = set(self.make_vocabulary(document))
+        bag_of_words = {}
 
-# print(find_features(documents[0].content))
+        for w in vocabulary:
+            bag_of_words[w] = (w in doc_words)
+
+        return bag_of_words
+
+    def freq_dist(self, vocabulary):
+        return nltk.FreqDist(vocabulary)
+
+    def top_words(self, freq_dist, number=500):
+        return list(freq_dist.keys())[:number]
+
+
+
+topics = DBH.session.query(Topic).all()
+
+# to make things quick
+topics = [topics[0], topics[20]]
+
+document_set_with_category = []
+def find_documents_for_topic_with_category(topic):
+    documents = []
+    for subtopic in topic.subtopics:
+        for document in subtopic.documents:
+            document_set_with_category.append([document, topic.title])
+
+
+for topic in topics:
+    find_documents_for_topic_with_category(topic)
+
+
+def get_distinct(original_list):
+    distinct_list = []
+    for each in original_list:
+        if each not in distinct_list:
+            distinct_list.append(each)
+    return distinct_list
 
 # import pdb; pdb.set_trace()
-'''
-http://scikit-learn.org/stable/modules/multiclass.html
-Multilabel classification assigns to each sample a set of target labels. This can be thought as predicting properties of a data-point that are not mutually exclusive, such as topics that are relevant for a document. A text might be about any of religion, politics, finance or education at the same time or none of these.
-http://scikit-learn.org/stable/auto_examples/plot_multilabel.html
-'''
+document_set_with_category = get_distinct(document_set_with_category)
+picked_item = document_set_with_category[:1]
+document_set_with_category.remove(picked_item[0])
+
+document_set = []
+# import pdb; pdb.set_trace()
+for doc, cat in document_set_with_category:
+    document_set.append(doc)
+
+# import pdb; pdb.set_trace()
+processor = WordProcessor(document_set)
+
+import random
+random.shuffle(document_set_with_category)
+
+featuresets = [(processor.bag_of_words(document), category) for (document, category) in document_set_with_category]
+for f, c in featuresets:
+    print(c)
+
+def split_list(featuresets):
+    half = int(len(featuresets)/2)
+    return featuresets[:half], featuresets[half:]
+
+training_set, testing_set  = split_list(featuresets)
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
