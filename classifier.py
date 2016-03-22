3fa8d5e71d5ddef20958905e38420dc78fe8cbf8
jackscotti <jacopojackscotti@gmail.com>
2016-03-22 09:26:56 +0000

    Build OneVSRest

--- classifier.py
+++ classifier.py
@@ -5,13 +5,11 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import string
+import random
 
 DBH = DBHandler(echo=False)
 
-
-
 class WordProcessor():
-
     def __init__(self, documents):
         self.documents = documents
         self.stemmer = PorterStemmer()
@@ -41,7 +39,7 @@ class WordProcessor():
 
     def remove_stopwords(self, words):
         # remove stop words
-        ADDITIONAL_STOP_WORDS = {"-", ".", ",", "if", "good", "what", "within", "https"}
+        ADDITIONAL_STOP_WORDS = {"-", ".", ",", "if", "good", "what", "within", "https", ".mb"}
         stop_words = set(stopwords.words("english"))
         return [w for w in words if not w in (stop_words | ADDITIONAL_STOP_WORDS)]
 
@@ -96,24 +94,31 @@ class WordProcessor():
         return list(freq_dist.keys())[:number]
 
 
+def random_topics():
+    topics = DBH.session.query(Topic).all()
+    random.shuffle(topics)
+    topics = topics[:3]
+    [print(topic.title) for topic in topics]
+    return topics
 
-topics = DBH.session.query(Topic).all()
+random_topics = random_topics()
 
-# to make things quick
-topics = [topics[0], topics[20]]
-
-document_set_with_category = []
 def find_documents_for_topic_with_category(topic):
-    documents = []
+    document_set_with_category = []
     for subtopic in topic.subtopics:
         for document in subtopic.documents:
             document_set_with_category.append([document, topic.title])
 
+    return document_set_with_category
 
-for topic in topics:
-    find_documents_for_topic_with_category(topic)
+def all_documents():
+    documents = []
+    for topic in random_topics:
+        documents = documents + find_documents_for_topic_with_category(topic)
 
+    return documents
 
+# some docs can appear in several topics, this should not be run
 def get_distinct(original_list):
     distinct_list = []
     for each in original_list:
@@ -121,32 +126,64 @@ def get_distinct(original_list):
             distinct_list.append(each)
     return distinct_list
 
-# import pdb; pdb.set_trace()
-document_set_with_category = get_distinct(document_set_with_category)
-picked_item = document_set_with_category[:1]
-document_set_with_category.remove(picked_item[0])
-
-document_set = []
-# import pdb; pdb.set_trace()
-for doc, cat in document_set_with_category:
-    document_set.append(doc)
-
-# import pdb; pdb.set_trace()
-processor = WordProcessor(document_set)
-
-import random
+document_set_with_category = get_distinct(all_documents())
 random.shuffle(document_set_with_category)
 
+# for feeding it to the classifier
+# picked_item = document_set_with_category[0]
+# document_set_with_category.remove(picked_item)
+
+# remove category
+processor = WordProcessor([doc for doc, cat in document_set_with_category])
+
 featuresets = [(processor.bag_of_words(document), category) for (document, category) in document_set_with_category]
-for f, c in featuresets:
-    print(c)
 
-def split_list(featuresets):
-    half = int(len(featuresets)/2)
-    return featuresets[:half], featuresets[half:]
+class OneVSRest():
+    def __init__(self, featuresets, topics):
+        self.featuresets = featuresets
+        self.topics = topics
 
-training_set, testing_set  = split_list(featuresets)
+    def split_list(self, featuresets):
+        half = int(len(featuresets)/2)
+        return featuresets[:half], featuresets[half:]
 
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
-classifier.show_most_informative_features(15)
+    def rename_label(self, featureset, topic):
+        # Rename to binary labels, Topic VS Rest
+        title = topic.title
+        if featureset[1] != title:
+            featureset = [featureset[0], "Rest"]
+
+        return featureset
+
+    def featuresets_for_one_vs_all(self):
+        # Builds a series of binary featureset
+        sets_of_featuresets = []
+
+        for topic in self.topics:
+            topic_featureset = []
+            for featureset in self.featuresets:
+                topic_featureset.append(self.rename_label(featureset, topic))
+            sets_of_featuresets.append(topic_featureset)
+
+        return sets_of_featuresets
+
+    def sets_of_training_testing_data(self):
+        # Split features sets in testing and training sets
+        training_testing = []
+        for featuresets in self.featuresets_for_one_vs_all():
+            training_set, testing_set = self.split_list(featuresets)
+            training_testing.append([training_set, testing_set])
+        return training_testing
+
+
+    def train_classifiers(self):
+        for training_set, testing_set in self.sets_of_training_testing_data():
+            classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+            print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+            print("'%s' VS '%s':" % tuple(classifier.labels()))
+            classifier.show_most_informative_features(20)
+
+
+ovs = OneVSRest(featuresets, random_topics)
+ovs.train_classifiers()
