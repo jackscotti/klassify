2cd8f809de9b1fd3d5942b98de5c26ea155f2188
jackscotti <jacopojackscotti@gmail.com>
2016-03-27 14:57:28 +0100

    Refactor word processor
    Processing 100 docs took:
    Before refactoring - 45s
    After refactoring - 0.75s

--- src/word_processor.py
+++ src/word_processor.py
@@ -7,7 +7,7 @@ class WordProcessor():
     def __init__(self, documents):
         self.documents = documents
         self.stemmer = PorterStemmer()
-        self.vocabulary = self.make_vocabulary()
+        self.vocabulary = self.top_words(self.freq_dist(self.make_vocabulary()))
 
     def extract_words(self, document=None):
         '''
@@ -18,66 +18,47 @@ class WordProcessor():
         else:
             documents = self.documents
 
-        words = []
+        return [token for doc in documents for token in word_tokenize(doc.content)]
 
-        for doc in documents:
-            # tokenize
-            tokens = word_tokenize(doc.content)
-
-            for token in tokens:
-                words.append(token)
-        return words
-
-    def downcase_words(self, words):
-        return [w.lower() for w in words]
-
-    def remove_stopwords(self, words):
-        # remove stop words
+    def process(self, vocabulary):
         ADDITIONAL_STOP_WORDS = {"-", ".", ",", "if", "good", "what", "within", "https", ".mb"}
         stop_words = set(stopwords.words("english"))
-        return [w for w in words if not w in (stop_words | ADDITIONAL_STOP_WORDS)]
 
-    def stem_words(self, words):
-        # stem
-        return [self.stemmer.stem(w) for w in words]
+        processed_words = []
 
-    def remove_long_words(self, words):
-        # remove anything that is not a word (js script that haven't been removed by previous parsing)
-        return [w for w in words if len(w) < 25]
+        for word in vocabulary:
+            # select only words shorter than 20 char
+            if len(word) < 20:
+                word = word.lower()
+                # do not select stopwords
+                if word not in (stop_words | ADDITIONAL_STOP_WORDS):
+                    # stem words
+                    word = self.stemmer.stem(word)
+                    # do not select words shorter than 2 characters
+                    if word.isalpha:
+                        if len(word) > 1:
+                            processed_words.append(word)
+                    else:
+                        processed_words.append(word)
 
-    def remove_short_words(self, words):
-        filtered_words = []
-        for w in words:
-            if w.isalpha:
-                if len(w) > 1:
-                    filtered_words.append(w)
-            else:
-                filtered_words.append(w)
-
-        return filtered_words
+        return processed_words
 
     def make_vocabulary(self, document=None):
         if document:
-            v = self.extract_words(document)
+            vocabulary = self.extract_words(document)
         else:
-            v = self.extract_words()
+            vocabulary = self.extract_words()
 
-        v = self.downcase_words(v)
-        v = self.remove_stopwords(v)
-        v = self.stem_words(v)
-        v = self.remove_short_words(v)
-        v = self.remove_long_words(v)
+        vocabulary = self.process(vocabulary)
 
-        return v
+        return vocabulary
 
     def bag_of_words(self, document):
-        vocabulary = self.top_words(self.freq_dist(self.vocabulary))
-
         doc_words = set(self.make_vocabulary(document))
         bag_of_words = {}
 
-        for w in vocabulary:
-            bag_of_words[w] = (w in doc_words)
+        for word in self.vocabulary:
+            bag_of_words[word] = (word in doc_words)
 
         return bag_of_words
 
