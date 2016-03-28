7ef95dc371a7145433692acb0b160757e44f93b6
jackscotti <jacopojackscotti@gmail.com>
2016-03-28 10:31:15 +0100

    Refactor DocumentImporter
    - Remove unneeded console output
    - Update remaining console output
    - Rename variables
    - Remove unnecessary comments

--- src/document_importer.py
+++ src/document_importer.py
@@ -6,18 +6,18 @@ import sqlalchemy
 import time
 
 class DocumentImporter(object):
-
     def __init__(self, db_name="klassify"):
         self.ROOT_URL = "https://www.gov.uk/api/search.json?reject_specialist_sectors=_MISSING"
         self.PAGE_URL = "https://www.gov.uk/api/search.json?reject_specialist_sectors=_MISSING&count=1000&start="
-        self.DBH = DBHandler(db_name)
+        self.DBH = DBHandler(db_name, echo=False)
 
     def api_response(self, url):
         time.sleep(0.15)
         return requests.get(url).json()
 
     def total_documents(self, document_data):
-        return document_data["total"]
+        self.document_count = document_data["total"]
+        return self.document_count
 
     def pages(self, number_of_documents):
         return math.ceil(number_of_documents / 1000)
@@ -31,7 +31,7 @@ class DocumentImporter(object):
         return urls
 
     def associate_document_with_subtopics(self, document, subtopics):
-        # remove duplicates by converting to a set and back to a list
+        # remove duplicates by converting topics to a set and then back to a list
         subtopics = set(subtopics)
         subtopics = list(subtopics)
         document.subtopics = subtopics
@@ -57,49 +57,39 @@ class DocumentImporter(object):
         subtopics = []
         for subtopic_data in subtopics_data:
             subtopic = self.DBH.session.query(Subtopic).filter_by(base_path=subtopic_data['link']).first()
-
-            if subtopic:
-                subtopics.append(subtopic)
-            # find out what to do when a topic is not found in db
-            # maybe create one?
+            if subtopic: subtopics.append(subtopic)
 
         return subtopics
 
     def run(self):
-        data_response = self.api_response(self.ROOT_URL)
-        number_of_documents = self.total_documents(data_response)
+        root_data = self.api_response(self.ROOT_URL)
+        number_of_documents = self.total_documents(root_data)
         pages = self.pages(number_of_documents)
         urls = self.urls(pages)
 
         count = 0
-
-        double_documents = []
+        duplicate_documents = []
 
         for url in urls:
-            data_response =  self.api_response(url)
-            documents_data = data_response['results']
+            list_of_documents =  self.api_response(url)
+            documents_data = list_of_documents['results']
             for document_data in documents_data:
                 document = self.make_document(document_data)
                 subtopics = self.find_subtopics(document_data)
                 if subtopics:
                     self.associate_document_with_subtopics(document, subtopics)
-                # find out what to do when a topic is not found in db
-                # maybe create one?
-
                 try:
                     self.DBH.session.add(document)
                     self.DBH.session.commit()
                 except sqlalchemy.exc.IntegrityError:
-                    double_documents.append(document.base_path)
+                    duplicate_documents.append(document.base_path)
                     self.DBH.session.rollback()
                 except:
                     self.DBH.session.rollback()
                     raise
-                print("Item number: ")
-                print(count)
+                if count % 250 == 0: print("Documents processed: %d/%d" % (count, self.document_count))
                 count = count + 1
 
         self.DBH.session.close()
 
-        print("Documents that appeared twice in the API and have been ignored:")
-        print(double_documents)
+        print("Documents with duplicates that have been ignored: %d", duplicate_documents)
