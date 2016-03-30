e56e19126e93de6955c60aa92934c99db66668b6
jackscotti <jacopojackscotti@gmail.com>
2016-03-30 17:15:15 +0100

    Refactor OVRHandler

--- src/ovr_handler.py
+++ src/ovr_handler.py
@@ -9,17 +9,9 @@ class OvrHandler():
     def __init__(self, featuresets):
         self.mlb = MultiLabelBinarizer()
         self.featuresets = featuresets
-        self.training_featuresets, self.testing_sets = self.split_list(featuresets)
         self._vectorizer = DictVectorizer(dtype=float, sparse=True)
+        self.X, self.y = self.prepare_scikit_x_and_y(self.featuresets)
         self.classifier = OneVsRestClassifier(MultinomialNB())
-
-    def split_list(self, featuresets):
-        half = int(len(featuresets)/2)
-        length = int(len(featuresets))
-        training_length = int(length / 100 * 80)
-        testing_length =  length - training_length
-
-        return featuresets[:training_length], featuresets[testing_length:]
 
     def prepare_scikit_x_and_y(self, labeled_featuresets):
         X, y = list(compat.izip(*labeled_featuresets))
@@ -34,13 +26,11 @@ class OvrHandler():
         return X, y
 
     def train_classifier(self):
-        X, y = self.prepare_scikit_x_and_y(self.featuresets)
-        self.classifier.fit(X, y)
+        self.classifier.fit(self.X, self.y)
 
     def test_classifier(self):
-        X, y = self.prepare_scikit_x_and_y(self.featuresets)
         scores = cross_validation.cross_val_score(
-            self.classifier, X, y, cv=10
+            self.classifier, self.X, self.y, cv=10
         )
         print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
@@ -48,11 +38,7 @@ class OvrHandler():
         doc, bag_of_words = doc_with_bag_of_words
         print("Predicting for:", doc.title)
         print("Item is labeled to:")
-        current_labels = []
-        for subtopic in doc.subtopics:
-            current_labels.append(subtopic.topic.title)
-
-        print(set(current_labels))
+        print(set(doc.topic_titles()))
         print("====> Predictions:")
 
         X = self._vectorizer.fit_transform(bag_of_words)
@@ -60,6 +46,7 @@ class OvrHandler():
         probabilities =  self.classifier.predict_proba(X)[0]
         named_classes = self.mlb.classes_
 
+        # If no labels are predicted for the item:
         if not 1 in predicted_labels:
             print("No label suggested for item")
             return
